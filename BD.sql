dfdc301486e05d666dc246497788487472fe0134
Luz Carcamo <luzcar5@gmail.com>
2023-08-12 17:28:42 -0500

    Archivos de la aplicacion

---
+++ BD.sql
@@ -0,0 +1,14 @@
+use empleado;
+show create table empleado;
+
+CREATE TABLE empleado (
+  id int not null auto_increment,
+  codigo varchar(11),
+  nombre varchar(50) ,
+  domicilio varchar(200) ,
+  telefono varchar(15) ,
+  correo_electronico varchar(45),
+  salario decimal,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
---
+++ src/principal/Principal.java
@@ -0,0 +1,19 @@
+
+package principal;
+
+import Controlador.ControladorEmpleado;
+import Modelo.ConsultasEmpleado;
+import Modelo.Empleado;
+import Vista.frmEmpleado;
+
+
+public class Principal {
+    public static void main(String[] args) {
+        Empleado mod=new Empleado();
+        ConsultasEmpleado modC=new ConsultasEmpleado();
+        frmEmpleado frm=new frmEmpleado();
+        ControladorEmpleado  crtl=new ControladorEmpleado(mod,modC,frm);
+        crtl.iniciar();
+        frm.setVisible(true);
+    }
+}
